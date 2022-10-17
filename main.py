44c82482e5c1e4687c75e5934abe29cf5a1e248a
Alberto martin <alberto@example.com>
2022-10-17 15:13:14 +0000

    pushing a build trigger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 from flask import Flask
-
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -10,6 +10,11 @@ def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello World!'
 
+
+@app.route('/name/<value>')
+def name(value):
+    val = {"value": value}
+    return jsonify(val)
 
 if __name__ == '__main__':
 
